184d6c423f1d30d8a8bee72b6f1db690ea8b5def
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-06-11 18:41:48 +0200

    validation moves

--- api2.py
+++ api2.py
@@ -39,8 +39,19 @@ class KingFigure(Figure):
                 allowed_move.append((x,y))
         return allowed_move
 
+    def validate_move(self, a, b):
+        self.a = a 
+        self.b = b
+        new_move = []
+        new_move.append(a)
+        new_move.append(b)
+        return new_move
+
+
+
 
 figures_dict = {}
+figures_dict_validation = {}
 
 figures = {"king": KingFigure}
 cords = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -66,17 +77,17 @@ class Chess(Resource):
         
         figure = figures.get(url_figure)
         args = figure(x, y).list_allowed_moves()
-        figures_dict["allowed_moves"] = str(args)
+        figures_dict["availableMoves"] = str(args)[1:-1]
         figures_dict["error"] = err
         figures_dict["figure"] = str(url_figure)
-        figures_dict["current_field"] = str(x) + "," + str(y)
+        figures_dict["currentField"] = str(x) + "," + str(y)
         return figures_dict
 
 
 class ChessValid(Resource):
-    def get(self, url_figure, x, y, a, b):
-        abort_if_figure_doesent_exist(url_figure)
-        if url_figure not in figures:
+    def get(self, url_figure_valid, x, y, a, b):
+        abort_if_figure_doesent_exist(url_figure_valid)
+        if url_figure_valid not in figures:
             err = "Figure is not valid..."
         else:
             err = None
@@ -86,20 +97,35 @@ class ChessValid(Resource):
         else:
             err = None
 
-        validation = str(a) + "," + str(b)
+        if a or b not in cords:
+            err = "Cords is not valid..."
+        else:
+            err = None
         
-        figure = figures.get(url_figure)
+        figure = figures.get(url_figure_valid)
         args = figure(x, y).list_allowed_moves()
-        figures_dict["move"] = validation
-        figures_dict["allowed_moves"] = str(args)
-        figures_dict["error"] = err
-        figures_dict["figure"] = str(url_figure)
-        figures_dict["current_field"] = str(x) + "," + str(y)
-        return figures_dict
+        validation = figure(x, y).validate_move(a, b)
+
+        if str(validation)[1:5] in str(args):
+            info = "valid"
+        else:
+            info = "invalid"
+
+        if info == "invalid":
+            err = "Current move is not permitted."
+        else:
+            err = None
+
+        figures_dict_validation["move"] = info
+        figures_dict_validation["figure"] = str(url_figure_valid)
+        figures_dict_validation["error"] = err
+        figures_dict_validation["currentField"] = str(x) + "," + str(y)
+        figures_dict_validation["destField"] = str(a) + "," + str(b)
+        return figures_dict_validation
 
 
 api.add_resource(Chess, "/api/v1/<string:url_figure>/<int:x>/<int:y>")
-api.add_resource(ChessValid, "/api/v1/<string:url_figure>/<int:x>/<int:y>/<int:a>/<int:b>")
+api.add_resource(ChessValid, "/api/v1/<string:url_figure_valid>/<int:x>/<int:y>/<int:a>/<int:b>")
 
 if __name__ == "__main__":
     app.run(debug=True)
