7f32b0262c9fb16e171aa0b038015b711348b393
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-06-12 10:53:20 +0200

    unittests

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ class Figure:
         self.x = x
         self.y = y
 
-    def list_available_moves(self):
-        pass
-
-    def validate_move(self, dest_field):
-        pass
+    def validate_move(self, a, b):
+        self.a = a 
+        self.b = b
+        new_move = []
+        new_move.append(a)
+        new_move.append(b)
+        return new_move
 
 
 class KingFigure(Figure):
@@ -37,36 +39,23 @@ class KingFigure(Figure):
                 allowed_move.append((x,y))
         return allowed_move
 
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move
-
 
 class PawnFigure(Figure):
     def __init__(self, x ,y):
         super().__init__(x ,y)
         
     def list_allowed_moves(self):
-        allowed_move = []
-        if self.y < 7:
-            allowed_move.append((self.x, self.y + 1))
-            allowed_move.append((self.x, self.y + 2))
+        if self.x < 8 or self.y < 8:
+            allowed_move = []
+            if self.y < 7:
+                allowed_move.append((self.x, self.y + 1))
+                allowed_move.append((self.x, self.y + 2))
+            else:
+                allowed_move.append((self.x, self.y-1))
+                allowed_move.append((self.x, self.y-2))
         else:
-            allowed_move.append((self.x, self.y-1))
-            allowed_move.append((self.x, self.y-2))
+            allowed_move = []
         return allowed_move
-
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move
 
 
 class KnightFigure(Figure):
@@ -84,33 +73,22 @@ class KnightFigure(Figure):
                 allowed_move.append((x,y))
         return allowed_move
 
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move
 
 class RookFigure(Figure):
     def __init__(self, x ,y):
         super().__init__(x ,y)
     def list_allowed_moves(self):
-        allowed_move = []
-        for Y in range(8):
-            if self.y != Y:
-                allowed_move.append((self.x, Y))
-        for X in range(8):
-            if self.x != X:
-                allowed_move.append((X, self.y))
+        if self.x < 8 or self.y < 8:
+            allowed_move = []
+            for Y in range(8):
+                if self.y != Y:
+                    allowed_move.append((self.x, Y))
+            for X in range(8):
+                if self.x != X:
+                    allowed_move.append((X, self.y))
+        else:
+            allowed_move = []
         return allowed_move    
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move   
 
 
 class BishopFigure(Figure):
@@ -118,41 +96,35 @@ class BishopFigure(Figure):
         super().__init__(x ,y)
 
     def list_allowed_moves(self):
-        allowed_move = set()
+        if self.x < 8 or self.y < 8:
+            allowed_move = set()
 
-        for i in range(1,8):
-            x = self.x+ i
-            y = self.y + i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x+ i
+                y = self.y + i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x - i
-            y = self.y - i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x - i
+                y = self.y - i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x+ i
-            y = self.y - i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x+ i
+                y = self.y - i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x- i
-            y = self.y + i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
-
+            for i in range(1,8):
+                x = self.x- i
+                y = self.y + i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
+        else:
+            allowed_move = []
         return list(allowed_move)
-
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move 
 
 
 class QueenFigure(Figure):
@@ -160,50 +132,46 @@ class QueenFigure(Figure):
         super().__init__(x ,y)
 
     def list_allowed_moves(self):
-        allowed_move = set()
-        for i in range(1,8):
-            x = self.x+ i
-            y = self.y + i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+        if self.x < 8 or self.y < 8:
+            allowed_move = set()
+            for i in range(1,8):
+                x = self.x + i
+                y = self.y + i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x - i
-            y = self.y - i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x - i
+                y = self.y - i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x+ i
-            y = self.y - i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x + i
+                y = self.y - i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        for i in range(1,8):
-            x = self.x- i
-            y = self.y + i
-            if 0 <= x < 8 and 0 <= y < 8:
-                allowed_move.add((x,y))
+            for i in range(1,8):
+                x = self.x - i
+                y = self.y + i
+                if 0 <= x < 8 and 0 <= y < 8:
+                    allowed_move.add((x,y))
 
-        # move horizontal - x the same, y changed
-        for Y in range(8):
-            if self.y != Y:
-                allowed_move.add((self.x, Y))
+            # move horizontal - x the same, y changed
+            for Y in range(8):
+                if self.y != Y:
+                    if 0 <= self.y < 8 and 0 <= y < 8:
+                        allowed_move.add((self.x, Y))
 
-        # move vertical - x changed, y the same
-        for X in range(8):
-            if self.x != X:
-                allowed_move.add((X, self.y))
+            # move vertical - x changed, y the same
+            for X in range(8):
+                if self.x != X:
+                    if 0 <= self.x < 8 and 0 <= y < 8:
+                        allowed_move.add((X, self.y))
+        else:
+            allowed_move = []
         return list(allowed_move)
-
-    def validate_move(self, a, b):
-        self.a = a 
-        self.b = b
-        new_move = []
-        new_move.append(a)
-        new_move.append(b)
-        return new_move 
-
 
 
 
@@ -223,7 +191,7 @@ figures = {
 
 def abort_if_figure_doesent_exist(url_figure):
     if url_figure not in figures:
-        abort(404, message="Figure is not valid...")
+        abort(404, message="Figure is not valid. You must choose from: pawn, rook, bishop, knight, queen, king")
 
 def abort_if_cords_doesent_exist(x, y, a, b):
     if x > 7 or y > 7 or a > 7 or b > 7:
@@ -258,8 +226,6 @@ class ChessValid(Resource):
 
         abort_if_figure_doesent_exist(url_figure_valid)
         abort_if_cords_doesent_exist(x, y, a, b)
-        if url_figure_valid not in figures:
-            err.append("Figure is not valid...")
 
         figure = figures.get(url_figure_valid)
         args = figure(x, y).list_allowed_moves()
